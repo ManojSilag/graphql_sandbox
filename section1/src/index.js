a5310697a073384a76cc7558b614a4d162818aa7
ManojSilag <manojsilag@gmail.com>
2020-06-12 19:04:45 +0530

    Adding graphql-yoga and making first query

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,1 +1,35 @@
-console.log(" hello world");
+import { GraphQLServer } from "graphql-yoga";
+
+const typedefs = `
+   type Query {
+       hello: String!
+       name: String!
+       loaction: String!
+       bio: String!
+   }
+`;
+const resolvers = {
+  Query: {
+    hello() {
+      return "Hello world from Manoj...";
+    },
+    name() {
+      return "silag Manoj";
+    },
+    loaction() {
+      return "Ahmednagar";
+    },
+    bio() {
+      return "Software Developer";
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs: typedefs,
+  resolvers: resolvers
+});
+
+server.start(() => {
+  console.log("Server is up...");
+});
---
+++ section1/src/myModule.js
@@ -0,0 +1,5 @@
+export default function add(a, b) {
+  return a + b;
+}
+
+export const sub = (a, b) => a - b;
