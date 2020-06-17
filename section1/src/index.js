a4df28efeb9b564b7c3c7515be5d14f4f49bdd15
ManojSilag <manojsilag@gmail.com>
2020-06-17 12:37:03 +0530

    data from client to server

--- section1/src/index.js
+++ section1/src/index.js
@@ -2,21 +2,59 @@ import { GraphQLServer } from "graphql-yoga";
 
 const typedefs = `
   type Query{
-    name: String!
-    isAuthenticated: Boolean!
-    age: Int!
-    sgpa: Float
-    key: ID!
+    add(num1: Float, num2: Float ): Float!
+    greeting(name: String, job: String): String!
+    me: User!
+    Post: Post!
   }
+
+  type User {
+    name: String!
+    id: ID!
+    email: String!
+    age: Int
+  }
+
+  type Post{
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
+  
 `;
 
 const resolvers = {
   Query: {
-    name() { return "Manoj"},
-    isAuthenticated(){ return false},
-    age(){ return 24 },
-    sgpa(){ return },
-    key(){ return 'fdsfsd12322'}
+    me() {
+      return {
+        name: "Manoj",
+        id: "ABCDEFG",
+        email: "manoj@SpeechGrammarList.com",
+        age: 12
+      };
+    },
+    Post() {
+      return {
+        id: "ABCDEFG",
+        title: "The way you lie",
+        body: "Loreum ispum ",
+        published: false
+      };
+    },
+    greeting(parent, args, ctx, info) {
+      // console.log(args);
+      if (args.name && args.job) {
+        return `Heloooooo ${args.name}! YOur job is ${args.job}`;
+      } else {
+        return "Hellooooo";
+      }
+    },
+    add(parent, args) {
+      const num1 = args.num1;
+      const num2 = args.num2;
+      return (num1 + num2);
+    }
   }
 };
 
