a1f5f4968a8115491cde221150aff45e7634caad
ManojSilag <manojsilag@gmail.com>
2020-06-22 12:33:44 +0530

    Mutation typedef and  resolver

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,5 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
-
+import { v4 as uuidv4 } from "uuid";
 //Demo user data
 const users = [
   {
@@ -94,6 +94,12 @@ const typedefs = `
     Post: Post!
   }
 
+  type Mutation{
+    createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author:ID!, post: ID!): Comment!
+  }
+
   type User {
     name: String!
     id: ID!
@@ -166,6 +172,63 @@ const resolvers = {
       return comments;
     }
   },
+
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some((user) => user.email === args.email);
+      if (emailTaken) {
+        throw new Error("Email taken");
+      }
+      const user = {
+        id: uuidv4(),
+        name: args.name,
+        email: args.email,
+        age: args.age
+      };
+      users.push(user);
+      return user;
+    },
+
+    createPost(parent, args, ctx, info) {
+      const userExist = users.some((user) => user.id === args.author);
+      if (!userExist) {
+        throw new Error("User Not Found");
+      }
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author
+      };
+      posts.push(post);
+      return post;
+    },
+
+    createComment(parent, args, ctx, info) {
+      const userExist = users.some((user) => user.id === args.author);
+      if (!userExist) {
+        throw new Error("User not found");
+      }
+      const postExistansPublished = posts.some((post) => {
+        const postId = post.id;
+        const postPublished = post.published;
+        return postPublished && args.post === postId;
+      });
+      if (!postExistansPublished) {
+        throw new Error("Post not found");
+      }
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post
+      };
+      comments.push(comment);
+      return comment;
+    }
+  },
+
   Post: {
     author(parent, args, ctx, info) {
       return users.find((user) => {
